9c8f29099c59dfb5c2505a2403cd88f7dbf88930
kimganghyeon <kimganghyeon6080@naver.com>
2024-06-08 11:37:58 +0900

    Add files via upload

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,8 @@
+from django.http import Http404
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 
@@ -21,10 +25,6 @@ def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
 
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render
-from .models import Question
 
 # index() 뷰를 단축 기능으로 작성 했을 때
 
@@ -35,13 +35,13 @@ from .models import Question
 
 #뷰를 업데이트 하는 기능
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("polls/index.html")
-#     context = {
-#         "latest_question_list": latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 # index() 뷰 하나를 호출했을 때, 시스템에 저장된 최소한 5 개의 투표 질문이 콤마로 분리되어, 발행일에 따라 출력됩니다.
