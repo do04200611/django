dcb8288280348c8175a523a50bf4c1ea8ae410f5
kimganghyeon <kimganghyeon6080@naver.com>
2024-06-08 16:30:58 +0900

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,152 @@
+C:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\views.py changed, reloading.
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+June 08, 2024 - 15:51:17
+Django version 5.0.6, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+[08/Jun/2024 15:51:23] "POST /polls/3/vote/ HTTP/1.1" 200 564
+[08/Jun/2024 15:51:37] "GET /polls/1/ HTTP/1.1" 200 835
+[08/Jun/2024 15:51:42] "POST /polls/1/vote/ HTTP/1.1" 302 0
+[08/Jun/2024 15:51:42] "GET /polls/1/results/ HTTP/1.1" 200 44
+C:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\views.py changed, reloading.
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+June 08, 2024 - 15:52:38
+Django version 5.0.6, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+C:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\views.py changed, reloading.
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+June 08, 2024 - 15:54:01
+Django version 5.0.6, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+C:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\views.py changed, reloading.
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+June 08, 2024 - 15:54:25
+Django version 5.0.6, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+C:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\views.py changed, reloading.
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+June 08, 2024 - 15:54:41
+Django version 5.0.6, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+C:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\views.py changed, reloading.
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+June 08, 2024 - 15:54:44
+Django version 5.0.6, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+[08/Jun/2024 15:59:11] "GET /polls/1/results/ HTTP/1.1" 200 262
+[08/Jun/2024 15:59:14] "GET /polls/1/ HTTP/1.1" 200 835
+[08/Jun/2024 15:59:46] "GET /polls/1/ HTTP/1.1" 200 835
+[08/Jun/2024 16:00:07] "POST /polls/1/vote/ HTTP/1.1" 302 0
+[08/Jun/2024 16:00:07] "GET /polls/1/results/ HTTP/1.1" 200 301
+[08/Jun/2024 16:01:34] "GET /polls/1/results/ HTTP/1.1" 200 345
+[08/Jun/2024 16:01:35] "GET /polls/1/results/ HTTP/1.1" 200 345
+[08/Jun/2024 16:01:36] "GET /polls/1/results/ HTTP/1.1" 200 345
+[08/Jun/2024 16:01:42] "GET /polls/1/results/ HTTP/1.1" 200 349
+[08/Jun/2024 16:01:42] "GET /polls/1/results/ HTTP/1.1" 200 349
+[08/Jun/2024 16:01:43] "GET /polls/1/results/ HTTP/1.1" 200 349
+[08/Jun/2024 16:01:50] "GET /polls/1/results/ HTTP/1.1" 200 396
+[08/Jun/2024 16:02:00] "GET /polls/1/results/ HTTP/1.1" 200 345
+[08/Jun/2024 16:02:17] "GET /polls/1/results/ HTTP/1.1" 200 393
+[08/Jun/2024 16:02:17] "GET /polls/1/results/ HTTP/1.1" 200 393
+[08/Jun/2024 16:02:24] "GET /polls/1/results/ HTTP/1.1" 200 389
+[08/Jun/2024 16:02:32] "GET /polls/1/results/ HTTP/1.1" 200 387
+[08/Jun/2024 16:02:44] "GET /polls/1/results/ HTTP/1.1" 200 389
+[08/Jun/2024 16:02:51] "GET /polls/1/results/ HTTP/1.1" 200 388
+[08/Jun/2024 16:02:52] "GET /polls/1/results/ HTTP/1.1" 200 388
+[08/Jun/2024 16:02:53] "GET /polls/1/results/ HTTP/1.1" 200 388
+[08/Jun/2024 16:03:24] "GET /polls/1/results/ HTTP/1.1" 200 423
+[08/Jun/2024 16:03:25] "GET /polls/1/ HTTP/1.1" 200 835
+[08/Jun/2024 16:03:28] "POST /polls/1/vote/ HTTP/1.1" 302 0
+[08/Jun/2024 16:03:28] "GET /polls/1/results/ HTTP/1.1" 200 423
+[08/Jun/2024 16:03:32] "POST /polls/1/vote/ HTTP/1.1" 302 0
+[08/Jun/2024 16:03:32] "GET /polls/1/results/ HTTP/1.1" 200 423
+[08/Jun/2024 16:03:34] "GET /polls/1/ HTTP/1.1" 200 835
+[08/Jun/2024 16:03:35] "POST /polls/1/vote/ HTTP/1.1" 302 0
+[08/Jun/2024 16:03:35] "GET /polls/1/results/ HTTP/1.1" 200 423
+[08/Jun/2024 16:03:36] "GET /polls/1/ HTTP/1.1" 200 835
+C:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\models.py changed, reloading.
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+June 08, 2024 - 16:04:18
+Django version 5.0.6, using settings 'mysite.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CTRL-BREAK.
+
+[08/Jun/2024 16:04:20] "POST /polls/1/vote/ HTTP/1.1" 200 928
+[08/Jun/2024 16:04:22] "POST /polls/1/vote/ HTTP/1.1" 302 0
+[08/Jun/2024 16:04:22] "GET /polls/1/results/ HTTP/1.1" 200 423
+[08/Jun/2024 16:04:26] "GET /polls/1/ HTTP/1.1" 200 835
+
+ *  복원된 기록 
+
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> & 'c:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\models.py'
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> ^C
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> ^C
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> ./ssh.sh
+./ssh.sh : './ssh.sh' 용어가 cmdlet, 함수, 스크립트 파일 또는 실행할 수 있는 프로그램 
+이름으로 인식되지 않습니다. 이름이 정확한지 확인하고 경로가 포함된 경우 경로가 올바른지 
+ 검증한 다음 다시 시도하십시오.
+위치 줄:1 문자:1
++ ./ssh.sh
++ ~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (./ssh.sh:String) [], CommandNotFoundExc  
+   eption
+    + FullyQualifiedErrorId : CommandNotFoundException
+
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> ls
+
+
+    디렉터리: C:\Users\kimga\OneDrive\바탕 화면\django\mysite
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2024-06-07  오후 11:15                mysite
+d-----      2024-06-08  오전 12:08                polls
+-a----      2024-06-08   오후 4:04         143360 db.sqlite3
+-a----      2024-06-07  오후 11:14            684 manage.py
+./ssh.sh : './ssh.sh' 용어가 cmdlet, 함수, 스크립트 파일 또는 실행할 수 있는 프로그램
+이름으로 인식되지 않습니다. 이름이 정확한지 확인하고 경로가 포함된 경우 경로가 올바른지       
+ 검증한 다음 다시 시도하십시오.
+위치 줄:1 문자:1
++ ./ssh.sh
++ ~~~~~~~~
+    + CategoryInfo          : ObjectNotFound: (./ssh.sh:String) [], CommandNotFoundExc        
+   eption
+    + FullyQualifiedErrorId : CommandNotFoundException
+
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> & 'c:\Users\kimga\OneDrive\바탕 화면\django\mysite\polls\models.py'
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> ^C
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite> ^C
+PS C:\Users\kimga\OneDrive\바탕 화면\django\mysite>
